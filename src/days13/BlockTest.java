9dd430865bf5e2f084ea7e6751c12a20f47e237b
junyong <bagjunyong14@gmail.com>
2024-07-17 17:47:04 +0900

    day13 조금 정리

--- src/days13/BlockTest.java
+++ src/days13/BlockTest.java
@@ -1,8 +1,8 @@
 package days13;
 
 public class BlockTest {
-//클래스 초기화 블럭은 프로그램과 동시에 출발
-//인스턴스화 될 때마다 인스턴스 초기화 블럭은 항상 출발
+//클래스 초기화 블럭은 프로그램과 동시에 출발 (1회)
+//인스턴스화 될 때마다 인스턴스 초기화 블럭은 매번 출발
 	
 	static double rate = 0.0; //명시적 초기화
 	String name;
@@ -14,7 +14,7 @@ public class BlockTest {
 	}
 	BlockTest(int age) {
 		//this.name = "익명";
-		System.out.println("디폴트 생성자 호출됨");
+		System.out.println("디폴트ㄴ 생성자 호출됨");
 	}
 	
 	//인스턴스 초기화 블럭
@@ -29,7 +29,7 @@ public class BlockTest {
 	static{
 		//클래스 변수를 초기화하기 위한 블럭
 		//**객체 생성과 상관없이 프로그램 시작할 때 한 번 초기화
-		//인스턴스 변수는 초기화 할 수 없다.
+		//여기서 인스턴스 변수는 초기화 할 수 없다. (제일 먼저 되니까)
 		System.out.println("클래스(static)초기화 블럭 {}.");
 		
 		rate =0.04; //static 붙은 애들 여기서 다시 초기화 됨
@@ -42,7 +42,7 @@ public class BlockTest {
 			BlockTest bt1 = new BlockTest();
 			
 			System.out.println("2");
-			BlockTest bt2 = new BlockTest();
+			BlockTest bt2 = new BlockTest(3);
 			
 			System.out.println(rate);
 			
